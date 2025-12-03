c163ba0446ce7a8a773d12f667387e75ef869388
Bakhrom Achilov <mailbak36@gmail.com>
2025-12-04 02:14:02 +0500

    users before finish

--- apps/users-service/src/adapters/persistence/user.repository.ts
+++ apps/users-service/src/adapters/persistence/user.repository.ts
@@ -9,47 +9,7 @@ import {
   AuthPlatform,
 } from '../../ports';
 import { IUser } from '../../ports/user.interface';
-
-interface UserRecord {
-  id: string;
-  uniqueId: string;
-  email: string | null;
-  phoneNumber: string | null;
-  telegramId: string | null;
-  googleId: string | null;
-  passwordHash: string;
-  fio: string;
-  avatar: string | null;
-  role: string;
-  userType: string | null;
-  status: string;
-  language: string;
-  isPrivacyPolicyAccepted: boolean;
-  isSubscribedNewsletter: boolean;
-  platform: string | null;
-  isVerified: boolean;
-  lastLoginAt: Date | null;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-interface UserWhereUniqueInput {
-  id?: string;
-  email?: string;
-  phoneNumber?: string;
-  telegramId?: string;
-  googleId?: string;
-  uniqueId?: string;
-}
-
-interface UserPrismaClient {
-  user: {
-    findUnique: (args: { where: UserWhereUniqueInput }) => Promise<UserRecord | null>;
-    findMany: (args: { where: { id: { in: string[] } } }) => Promise<UserRecord[]>;
-    create: (args: { data: Partial<UserRecord> }) => Promise<UserRecord>;
-    update: (args: { where: { id: string }; data: Partial<UserRecord> }) => Promise<UserRecord>;
-  };
-}
+import { UserRecord, UserPrismaClient } from './prisma-types';
 
 @Injectable()
 export class PrismaUserRepository implements IUserRepository {
--- apps/users-service/src/adapters/persistence/users-aggregate.store.ts
+++ apps/users-service/src/adapters/persistence/users-aggregate.store.ts
@@ -1,72 +1,14 @@
-import { Inject, Injectable, Optional } from '@nestjs/common';
-import {
-  AggregateStore,
-  AggregateRestorer,
-  IEventStore,
-  OutboxService,
-  SnapshotService,
-  DomainEvent,
-} from '@flexobo/core';
+import { Injectable } from '@nestjs/common';
+import { GenericAggregateStore, EventSourcedAggregate } from '@flexobo/core';
 
-import { User } from '../../domain/user.aggregate';
+import { User, UserSnapshotData } from '../../domain/user.aggregate';
 import { IUserAggregateStore } from '../../ports/user-store.port';
-import { AuthPlatform, UserRole, UserStatus, UserType } from '../../ports';
-
-interface UserSnapshotData {
-  id: string;
-  uniqueId?: string;
-  email?: string;
-  phoneNumber?: string;
-  telegramId?: string;
-  passwordHash?: string;
-  fio?: string;
-  avatar?: string;
-  role?: UserRole;
-  userType?: UserType;
-  status?: UserStatus;
-  language?: string;
-  isPrivacyPolicyAccepted?: boolean;
-  isSubscribedNewsletter?: boolean;
-  platform?: AuthPlatform | null;
-  isVerified?: boolean;
-  lastLoginAt?: Date | null;
-  isLoggedIn?: boolean;
-}
 
 @Injectable()
 export class UserAggregateStore
-  extends AggregateStore<User, UserSnapshotData>
+  extends GenericAggregateStore<User, UserSnapshotData>
   implements IUserAggregateStore
 {
-  constructor(
-    @Inject('IEventStore') eventStore: IEventStore,
-    outboxService: OutboxService,
-    @Optional() snapshotService?: SnapshotService
-  ) {
-    super(eventStore, outboxService, snapshotService);
-  }
-
-  protected getAggregateType(): string {
-    return 'User';
-  }
-
-  protected getAggregateRestorer(): AggregateRestorer<User, UserSnapshotData> {
-    return {
-      fromSnapshot(
-        snapshotData: UserSnapshotData,
-        snapshotVersion: number,
-        subsequentEvents: DomainEvent[]
-      ): User {
-        return User.fromSnapshot(
-          snapshotData,
-          snapshotVersion,
-          subsequentEvents
-        );
-      },
-
-      fromEvents(events: DomainEvent[]): User {
-        return User.fromEvents(events);
-      },
-    };
-  }
+  protected readonly aggregateType = 'User';
+  protected readonly aggregateClass: EventSourcedAggregate<User, UserSnapshotData> = User;
 }
